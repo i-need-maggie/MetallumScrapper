645ef05673f3da30c07649c1d4e11446ffd52105
i-need-maggie <prashant.human.ai@gmail.com>
2024-06-29 15:20:38 +0530

    changed the func name to run to avoid conflict

--- main.py
+++ main.py
@@ -8,23 +8,24 @@ def safe_write(filename, folder_path=None):
     else:
         return os.path.join(os.makedirs(os.path.dirname(folder_path), exist_ok=True),filename)
     
-def main(base_url, write_path=None):
+def run(url, write_path=None, pull_discogs=False):
     
-    band_pool = BandCrawler(base_url=base_url, wait_time=2)
+    band_pool = BandCrawler(base_url=url, wait_time=2)
     band_pool._crawl_pool_meta()
     pool = band_pool.meta.copy()
-    pool.to_csv(safe_write("Bands.csv",folder_path), index=False)
+    pool.to_csv(safe_write("Bands.csv",write_path), index=False)
 
-    pbc = PoolDiscogCrawler(pool, page_wait=1)
-    out = pbc.thread_crawl(return_merged=True)
-    out = numeric_review(out, rev_col='Reviews', expand=True)
-    out.to_csv(safe_write('Discography.csv',folder_path), index=False, encoding='utf-16')
+    if pull_discogs:
+        pbc = PoolDiscogCrawler(pool, page_wait=1)
+        out = pbc.thread_crawl(return_merged=True)
+        out = numeric_review(out, rev_col='Reviews', expand=True)
+        out.to_csv(safe_write('Discography.csv',write_path), index=False, encoding='utf-16')
 
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    BASE_URL = "https://www.metal-archives.com/search/advanced/searching/bands?bandName=&genre=*funeral*&country=&yearCreationFrom=&yearCreationTo=&bandNotes=&status=&themes=&location=&bandLabelName=#bands"
-    TO_PATH = None
+#     BASE_URL = "https://www.metal-archives.com/search/advanced/searching/bands?bandName=&genre=*funeral*&country=&yearCreationFrom=&yearCreationTo=&bandNotes=&status=&themes=&location=&bandLabelName=#bands"
+#     TO_PATH = None
 
-    main(base_url=BASE_URL, write_path=TO_PATH)
+#     run(url=BASE_URL, write_path=TO_PATH, pull_discogs=True)
